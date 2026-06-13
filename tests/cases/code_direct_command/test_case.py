1a5e5dd63ff3afca769d4cbef79e7502168dbd88
AntonC9018 <anton.curm@gmail.com>
2026-06-13 14:22:35 +0300

    Revert insertCode macros

--- tests/cases/code_direct_command/test_case.py
+++ tests/cases/code_direct_command/test_case.py
@@ -7,9 +7,9 @@ from tests.lib.assertions import (
 from tests.lib import pdf
 
 
-def test_insert_code_command_accepts_direct_braced_body_without_percent(build):
+def test_code_environments_accept_direct_braced_body_without_percent(build):
     assert_build_succeeded(build)
-    assert_pdf_contains(build, "Direct command reference: 1.1.")
+    assert_pdf_contains(build, "Direct environment reference: 1.1.")
     assert_pdf_contains(build, "Codul 1.1.")
     assert_pdf_contains(build, "#include")
     assert_pdf_contains(build, "std::cout")
@@ -17,7 +17,7 @@ def test_insert_code_command_accepts_direct_braced_body_without_percent(build):
     assert_pdf_contains(build, "Regular text")
     assert_pdf_contains(build, "With braces")
     assert_pdf_contains(build, "Hash #")
-    assert_pdf_not_contains(build, "Plain command block: Codul")
+    assert_pdf_not_contains(build, "Plain unnumbered code block: Codul")
     assert_log_not_contains(build, "Package config Warning")
 
     text = pdf.pdf_text(build.pdf_path)
